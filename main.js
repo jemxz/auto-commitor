0ca37c5f652a0546903e3f78ba0398beff6c2979
Jemail <jemxz.omer@gmail.com>
2021-02-17 17:06:43 +0300

    its tedious

---
+++ main.js
@@ -0,0 +1,25 @@
+const { app, BrowserWindow } = require('electron') 
+try {
+    require('electron-reloader')(module)
+  } catch (_) {}
+
+
+function createWindow() {
+    // Create the browser window.
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        nodeIntegration: true,
+      },
+    })
+  
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+  }
+  
+  app.whenReady().then(createWindow)
+
+// In this file, you can include the rest of your 
+// app's specific main process code. You can also 
+// put them in separate files and require them here. 
