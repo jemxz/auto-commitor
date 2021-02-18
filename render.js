c03fc4202fd107e3333b38ca5c854a45819b6a5c
Jemail <jemxz.omer@gmail.com>
2021-02-18 21:19:00 +0300

    First Message

---
+++ render.js
@@ -0,0 +1,36 @@
+var fs = require('fs');
+var cmd=require('node-cmd');
+
+
+
+
+ function runForever() { 
+      fs.readFile('index.txt', 'utf-8', function(err, data) {
+         if (err) throw err;
+         var arr = fs.readFileSync('index.txt', 'utf-8').split('\n')
+         
+         if (arr[0] === "name"){
+             var newValue = data.replace(/name/gim, 'email');
+         } else if (arr[0] === "email"){
+             var newValue = data.replace(/email/gim, 'name');
+         }
+      
+         fs.writeFile('index.txt', newValue, 'utf-8', function(err, data) {
+             if (err) throw err;
+             
+         })
+     })
+
+    cmd.runSync('git add .');
+    cmd.runSync('git commit -m "First Message"');
+    cmd.runSync('git push origin master')
+
+    console.log("task complete ...");
+ }
+
+runForever()
+
+// setInterval(runForever, 60000)
+// setInterval(runForever, 43200000)
+
+ 
